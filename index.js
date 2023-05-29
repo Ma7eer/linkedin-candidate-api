5e32d7bd1c92e856443b6362ed741ff33124652f
Maher Alkendi <maheralkendi@Mahers-MacBook-Air.local>
2023-05-29 22:48:42 +0400

    add a simple get route

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+app.get("/", (req, res) => {
+    res.send("Hello world");
+})
+
 app.listen(3000, () => {
     console.log(`Listening on port ${PORT}`);
 })
