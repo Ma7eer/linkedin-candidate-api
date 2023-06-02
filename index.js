6a94e31c152cc24320485b964ceff72753ea91ae
Maher Alkendi <maheralkendi@Mahers-MacBook-Air.local>
2023-06-02 12:31:02 +0400

    Add: endpoint to add a candidate

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const { Directus } = require('@directus/sdk');
 const PORT = 3000;
 const DIRECTUS_INSTANCE_URL = 'http://localhost:8055';
 
+app.use(express.json()) 
+
 const directus = new Directus(DIRECTUS_INSTANCE_URL);
 
 app.get('/candidates', async (req, res) => {
@@ -13,6 +15,21 @@ app.get('/candidates', async (req, res) => {
         // NOTE: No auth because the collection has been set to public on Directus
         const candidates = await directus.items('candidate').readByQuery({ sort: ['id'] });
         res.json({'candidates': candidates.data});
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+app.post('/candidate', async (req, res) => {
+    try {
+        // const sampleCandidate = {
+        //     name: 'Person 1',
+        //     email: 'person.email@doman.com',
+        //     linkedin_url: 'https://www.linkedin.com/in/',
+        //     contact_info_url: 'https://www.linkedin.com/in/'
+        // }
+        await directus.items('candidate').createOne(req.body);
+        res.json({message: 'candidate successful added', requestBody: req.body})
     } catch (error) {
         console.log(error);
     }
